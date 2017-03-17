5fbbe712c0af0eba1cc3eb5260fd18c0d9377c29
Roland Maio <rolandmaio38@gmail.com>
2017-03-17 09:46:18 -0400

    - Added classes for an FBS Element and a group of FBS Elements. - Updated instance attributes to reflect this.

--- effayoh/mungers/fbs.py
+++ effayoh/mungers/fbs.py
@@ -33,6 +33,32 @@ class FBSItemGroup(frozenset):
         self.attr_name = attr_name
 
 
+class FBSElement(tuple):
+
+    __slots__ = []
+    object_pool = {}
+
+    def __new__(cls, element, code):
+        tup = (element, code)
+        if tup in FBSElement.object_pool:
+            return FBSElement.object_pool[tup]
+        else:
+            obj = super().__new__(cls, tup)
+            FBSElement.object_pool[obj] = obj
+            return obj
+
+
+class FBSElementGroup(frozenset):
+
+    __slots__ = ["attr_name"]
+
+    def __new__(cls, elements, attr_name):
+        return super().__new__(cls, elements)
+
+    def __init__(self, elements, attr_name):
+        self.attr_name = attr_name
+
+
 class FBSMunger:
     """
     Data munger for the FAO Food Balance Sheet data.
@@ -42,45 +68,53 @@ class FBSMunger:
     def __init__(self, political_rectifier):
         self.years = None
         self.items = set()
-        self.item_attrs = set()
-        self.groups = set()
-        self.item_conversions = {}
-        self.group_conversions = {}
+        self.elements = set()
+        self.element_attrs = set()
+        self.element_groups = set()
+        self.element_conversions = {}
+        self.element_group_conversions = {}
         self.political_rectifier = political_rectifier
 
     def set_years(self, years):
         self.years = years
 
-    def add_item(self, item, mk_attr=False):
+    def add_item(self, item):
         self.items.add(item)
-        if mk_attr:
-            self.item_attrs.add(item)
 
     def add_items(self, items):
         for item in items:
             self.add_item(item)
 
-    def add_item_group(self, group):
-        self.groups.add(group)
-        self.add_items(group)
+    def add_element(self, element, mk_attr=False):
+        self.elements.add(element)
+        if mk_attr:
+            self.element_attrs.add(element)
 
-    def add_item_groups(self, groups):
+    def add_elements(self, elements):
+        for element in elements:
+            self.add_element(element)
+
+    def add_element_group(self, group):
+        self.element_groups.add(group)
+        self.add_elements(group)
+
+    def add_element_groups(self, groups):
         for group in groups:
-            self.add_item_group(group)
+            self.add_element_group(group)
 
-    def set_item_conversion(self, item, conversion):
-        if not isinstance(item, FBSItem):
-            raise TypeError("item must be an FBSItem.")
+    def set_element_conversion(self, element, conversion):
+        if not isinstance(element, FBSElement):
+            raise TypeError("element must be an FBSElement")
         if not callable(conversion):
-            raise TypeError("conversion must be a function.")
-        self.item_conversions[item] = conversion
+            raise TypeError("conversion must be a callable.")
+        self.element_conversions[element] = conversion
 
-    def set_group_conversion(self, group, conversion):
-        if not isinstance(group, FBSItemGroup):
-            raise TypeError("group must be an FBSItemGroup.")
+    def set_element_group_conversion(self, group, conversion):
+        if not isinstance(group, FBSElementGroup):
+            raise TypeError("group must be an FBSElementGroup")
         if not callable(conversion):
-            raise TypeError("conversion must be a function.")
-        self.group_conversions[group] = conversion
+            raise TypeError("conversion must be a callable.")
+        self.element_group_conversions[group] = conversion
 
     def munge(self):
         """
@@ -88,34 +122,42 @@ class FBSMunger:
         """
         data = self.get_raw_data()
 
-        # Apply the item-wise conversions.
+        # Apply the element-wise conversions.
         for political_entity, items in data.items():
-            for item, years in items.items():
-                mean = sum(years.values()) / len(self.years)
-                func = self.item_conversions.get(item, lambda x: x)
-                value = func(mean)
-                items[item] = value
+            for item, elements in items.items():
+                for element, years in elements.items():
+                    mean = sum(years.values()) / len(self.years)
+                    func = self.element_conversions.get(item, lambda x: x)
+                    value = func(mean)
+                    elements[element] = value
 
-                if item in self.item_attrs:
-                    name, _ = item
-                    self.set_network_attr(political_entity, name, value)
+                    if element in self.element_attrs:
+                        name, _ = item
+                        self.set_network_node_attr(political_entity,
+                                                   name,
+                                                   value)
 
         # Apply the group-wise conversions.
-        for group in self.groups:
+        for group in self.element_groups:
             for political_entity, items in data.items():
-                func = self.group_conversions.get(
-                    group,
-                    lambda x: sum(x.values())
-                )
-                value = func({item: items[item] for item in items
-                                                if item in group})
-                self.set_network_attr(political_entity,
-                                      group.attr_name,
-                                      value)
+                for item, elements in items.items():
+                    func = self.element_group_conversions.get(
+                        group,
+                        lambda x: sum(x.values())
+                    )
+                    value = func({element: elements[element]
+                                  for element in elements
+                                  if element in group})
+                    self.set_network_node_attr(political_entity,
+                                               group.attr_name,
+                                               value)
 
     def get_raw_data(self):
         """
         Return a dict of country data for the selected items and years.
+
+        The dict is four dimensional keying on Area Code, Item, Element
+        and Year.
         """
         data_path = os.path.join(FAOSTAT_DIR,
                                  "food-balance-sheets",
@@ -130,14 +172,19 @@ class FBSMunger:
 
             for row in reader:
 
-                item, code = row["Item"], row["Item Code"]
-                if not (item, code) in self.items:
+                item = FBSItem(row["Item"], row["Item Code"])
+                if not item in self.items:
                     continue
 
-                # The row has one of the target item codes but it might
-                # not have a value for any of the years. We do the
-                # check here to avoid creating chains of dicts in the
-                # data that ultimately have no values.
+                element = FBSElement(row["Element"], row["Element Code"])
+                if not element in self.elements:
+                    continue
+
+                # The row has one of the target item codes and one of
+                # the target element codes but it might not have a value
+                # for any of the years. We do the check here to avoid
+                # creating chains of dicts in the data that ultimately
+                # have no values.
                 years_values = []
                 for year, field in years_fields:
                     try:
@@ -151,7 +198,8 @@ class FBSMunger:
 
                 political_entity = row["Area Code"]
                 item_dict = data.setdefault(political_entity, {})
-                year_dict = item_dict.setdefault((item, code), {})
+                elem_dict = item_dict.setdefault(item, {})
+                year_dict = elem_dict.setdefault(element, {})
 
                 for year, value in year_values:
                     year_dict[year] = value
@@ -160,9 +208,9 @@ class FBSMunger:
 
         return data
 
-    def set_network_attr(self, political_entity, name, value):
-        node = self.get_node(political_entity)
-        node[name] = value
-
-    def get_node(self, political_entity):
-        return self.political_rectifier.get_node(political_entity)
+    def set_network_node_attr(self, country, name, value):
+        self.political_rectifier.set_network_node_attr(
+            country,
+            name,
+            value
+        )
