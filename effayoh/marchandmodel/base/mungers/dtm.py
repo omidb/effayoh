e71ab7f1b103af93b6bdbcb4d83f4750d3a29bf2
Roland Maio <rolandmaio38@gmail.com>
2017-03-23 15:59:10 -0400

    Tidied up the module.
    - Collected the comments for missing items.
    - Removed extraneous BaseDTMMunger class attribute.
    - Removed extraneous make_converter function.

--- effayoh/marchandmodel/base/mungers/dtm.py
+++ effayoh/marchandmodel/base/mungers/dtm.py
@@ -11,6 +11,27 @@ from effayoh.mungers.dtm import (
 
 EXPORT_TONNES = DTMElement(element="Export Quantity", code="5910")
 
+# Define the items used in the base Marchand model which are enumerated
+# in Table 2 of the Supplementary Data. Several of the items listed in
+# the table do not occur in the definitions and standards associated
+# with the detailed trade matrix, they are:
+#
+# Crop name - item code
+# Rice, paddy - 27
+# Rice Husked - 28
+# Milled/Husked Rice - 29
+# Rice Milled - 31
+# Rice Broken - 32
+# Rice Flour - 38
+# Pot Barlety - 45
+# Barley Flour and Grits - 48
+# Malt Extract - 50
+# Flour of Rye - 72
+# Flour of Millet - 80
+# Flour of Sorghum - 84
+# Flour of Buckwheat - 89
+# Quinoa - 92
+# Cereals, nes - 108
 WHEAT = DTMItem(item="Wheat", code="15")
 FLOUR_OF_WHEAT = DTMItem(item="Flour, wheat", code="16")
 MACARONI = DTMItem(item="Macaroni", code="18")
@@ -19,63 +40,32 @@ BREAD = DTMItem(item="Bread", code="20")
 BULGUR = DTMItem(item="Bulgur", code="21")
 PASTRY = DTMItem(item="Pastry", code="22")
 RICE = DTMItem(item="Rice - total  (Rice milled equivalent)", code="30")
-# These items do not appear in the FAOSTAT detailed trade matrix
-# definitions and standards.
-#RICE_PADDY = DTMItem(item="", code="27")
-#RICE_HUSKED = DTMItem(item="", code="28")
-#MILLED_HUSKED_RICE = DTMItem(item="", code="29")
-#RICE_MILLED = DTMItem(item="", code="31")
-#RICE_BROKEN = DTMItem(item="", code="32")
-#RICE_FLOUR = DTMItem(item="", code="38")
 BREAKFAST_CEREALS = DTMItem(item="Cereals, breakfast", code="41")
 BARLEY = DTMItem(item="Barley", code="44")
-# Not found.
-# POT_BARLEY = DTMItem(item="", code="45")
 BARLEY_PEARLED = DTMItem(item="Barley, pearled", code="46")
-# Not found.
-# BARLEY_FLOUR_AND_GRITS = DTMItem(item="", code="48")
 MALT = DTMItem(item="Malt", code="49")
-# Not found.
-# MALT_EXTRACT = DTMItem(item="", code="50")
 MAIZE = DTMItem(item="Maize", code="56")
 GERM_OF_MAIZE = DTMItem(item="Germ, maize", code="57")
 FLOUR_OF_MAIZE = DTMItem(item="Flour, maize", code="58")
 POPCORN = DTMItem(item="Popcorn", code="68")
 RYE = DTMItem(item="Rye", code="71")
-# Not found.
-# FLOUR_OF_RYE = DTMItem(item="", code="72")
 OATS = DTMItem(item="Oats", code="75")
 OATS_ROLLED = DTMItem(item="Oats rolled", code="76")
 MILLET = DTMItem(item="Millet", code="79")
-# Not found.
-# FLOUR_OF_MILLET = DTMItem(item="", code="80")
 SORGHUM = DTMItem(item="Sorghum", code="83")
-# Not found.
-# FLOUR_OF_SORGHUM = DTMItem(item="", code="84")
 BUCKWHEAT = DTMItem(item="Buckwheat", code="89")
-# Not found.
-# FLOUR_OF_BUCKWHEAT = DTMItem(item="", code="90")
-# QUINOA = DTMItem(item="", code="92")
 FONIO = DTMItem(item="Fonio", code="94")
 FLOUR_OF_FONIO = DTMItem(item="Flour, fonio", code="95")
 TRITICALE = DTMItem(item="Triticale", code="97")
 CANARY_SEED = DTMItem(item="Canary seed", code="101")
 MIXED_GRAIN = DTMItem(item="Grain, mixed", code="103")
 FLOUR_OF_MIXED_GRAIN = DTMItem(item="Flour, mixed grain", code="104")
-# Not found.
-# CEREALS_NES = DTMItem(item="", code="108")
 INFANT_FOOD = DTMItem(item="", code="109")
 WAFERS = DTMItem(item="Infant food", code="110")
 FLOUR_OF_CEREALS = DTMItem(item="Flour, cereals", code="111")
 CEREAL_PREPARATIONS_NES = DTMItem(item="Cereal preparations, nes", code="113")
 MIXES_AND_DOUGHS = DTMItem(item="Mixes and doughs", code="114")
 FOOD_PREP_FLOUR_MALT_EXTRACT = DTMItem(item="Food preparations, flour, malt extract", code="115")
-
-
-def make_converter(factor):
-    def converter(value):
-        return factor*value
-    return converter
 
 
 # Conversion factors (10^6 kcal / ton)
@@ -144,46 +134,8 @@ calorie_cereals_exports = (
     calorie_cereals_exports_converter,  # conversion function
 )
 
+
 class BaseDTMMunger(DTMMunger):
-
-
-    """
-    item_element_edges = [
-        (WHEAT, EXPORT_TONNES, make_converter(3.34)),
-        (FLOUR_OF_WHEAT, EXPORT_TONNES, make_converter(3.64)),
-        (MACARONI, EXPORT_TONNES, make_converter(3.67)),
-        (BREAD, EXPORT_TONNES, make_converter(2.49)),
-        (BULGUR, EXPORT_TONNES, make_converter(3.45)),
-        (PASTRY, EXPORT_TONNES, make_converter(3.69)),
-        (RICE, EXPORT_TONNES, make_converter(3.6)),
-        (BREAKFAST_CEREALS, EXPORT_TONNES, make_converter(3.89)),
-        (BARLEY, EXPORT_TONNES, make_converter(3.32)),
-        (BARLEY_PEARLED, EXPORT_TONNES, make_converter(3.46)),
-        (MALT, EXPORT_TONNES, make_converter(3.68)),
-        (MAIZE, EXPORT_TONNES, make_converter(3.56)),
-        (GERM_OF_MAIZE, EXPORT_TONNES, make_converter(3.73)),
-        (FLOUR_OF_MAIZE, EXPORT_TONNES, make_converter(3.63)),
-        (POPCORN, EXPORT_TONNES, make_converter(3.56)),
-        (RYE, EXPORT_TONNES, make_converter(3.19)),
-        (OATS, EXPORT_TONNES, make_converter(3.85)),
-        (OATS_ROLLED, EXPORT_TONNES, make_converter(3.84)),
-        (MILLET, EXPORT_TONNES, make_converter(3.40)),
-        (SORGHUM, EXPORT_TONNES, make_converter(3.43)),
-        (BUCKWHEAT, EXPORT_TONNES, make_converter(3.30)),
-        (FONIO, EXPORT_TONNES, make_converter(3.38)),
-        (FLOUR_OF_FONIO, EXPORT_TONNES, make_converter(3.55)),
-        (TRITICALE, EXPORT_TONNES, make_converter(3.27)),
-        (CANARY_SEED, EXPORT_TONNES, make_converter(3.88)),
-        (MIXED_GRAIN, EXPORT_TONNES, make_converter(3.40)),
-        (FLOUR_OF_MIXED_GRAIN, EXPORT_TONNES, make_converter(3.64)),
-        (INFANT_FOOD, EXPORT_TONNES, make_converter(3.68)),
-        (WAFERS, EXPORT_TONNES, make_converter(4.39)),
-        (FLOUR_OF_CEREALS, EXPORT_TONNES, make_converter(3.64)),
-        (CEREAL_PREPARATIONS_NES, EXPORT_TONNES, make_converter(3.64)),
-        (MIXES_AND_DOUGHS, EXPORT_TONNES, make_converter(3.93)),
-        (FOOD_PREP_FLOUR_MALT_EXTRACT, EXPORT_TONNES, make_converter(3.77))
-    ]
-    """
 
     def __init__(self, political_rectifier):
         super().__init__(self, political_rectifier)
