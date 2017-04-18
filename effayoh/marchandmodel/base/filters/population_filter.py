a14d8220c60ece52bbf65560003548ca069472b3
Roland Maio <rolandmaio38@gmail.com>
2017-04-18 13:50:02 -0400

    Manually excluded countries that had no UN population data

--- effayoh/marchandmodel/base/filters/population_filter.py
+++ effayoh/marchandmodel/base/filters/population_filter.py
@@ -12,6 +12,7 @@ import csv
 from effayoh.mungers import FAOCountry
 from effayoh.mungers.fbs import FBSItem, FBSElement
 
+from effayoh.rectification.political_entities import FAOPolitEnt
 from effayoh.resources.faostat import map as map_
 
 from effayoh.util import FAOSTAT_DIR
@@ -27,6 +28,28 @@ POPULATION_ITEM = FBSItem(item="Population", code="2501")
 POPULATION_ELEMENT = FBSElement(element="Total Population - Both sexes",
                                 code="511")
 
+# Some countries do not have population data reported in the food
+# balance sheets and must be manually excluded.
+EXCLUDED_COUNTRIES = [
+    map_[FAOCountry("Seychelles", "196")],
+    FAOPolitEnt.MALTA,
+    FAOPolitEnt.COOK_ISLANDS,
+    FAOPolitEnt.ANDORRA,
+    FAOPolitEnt.FAROE_ISLANDS,
+    FAOPolitEnt.GUAM,
+    FAOPolitEnt.GREENLAND,
+    FAOPolitEnt.FRENCH_GUIANA,
+    FAOPolitEnt.GIBRALTAR,
+    FAOPolitEnt.CHRISTMAS_ISLAND,
+    FAOPolitEnt.CAYMAN_ISLANDS,
+    FAOPolitEnt.GUADELOUPE,
+    FAOPolitEnt.COCOS,
+    FAOPolitEnt.FALKLAND_ISLANDS,
+    FAOPolitEnt.ARUBA,
+    FAOPolitEnt.LIECNTENSTEIN,
+    FAOPolitEnt.BRITISH_INDIAN_OCEAN_TERRITORY,
+]
+
 years = list(range(2006, 2010+1))
 
 
@@ -39,7 +62,7 @@ class FAOSTATPopulationFilter:
         """
 
         self.country_populations = {}
-        self.excluded_countries = set()
+        self.excluded_countries = set(EXCLUDED_COUNTRIES)
         years_fields = [(year, "Y" + str(year)) for year in years]
 
         with open(data_path, encoding="latin1") as fh:
@@ -94,6 +117,8 @@ class FAOSTATPopulationFilter:
 
                 if population <= threshold:
                     self.excluded_countries.add(effpent)
+
+        self.excluded_countries.remove(FAOPolitEnt.LUXEMBOURG)
 
     def excludes(self, effpent):
         return effpent in self.excluded_countries
